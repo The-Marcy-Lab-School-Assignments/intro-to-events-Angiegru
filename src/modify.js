5d15186fd9d591c83a00e5067ee9ee3421e6d837
Angiegru <angiegrullon01@gmail.com>
2023-12-10 15:01:59 -0500

    Question 4 Complete

--- src/modify.js
+++ src/modify.js
@@ -15,27 +15,40 @@ const clickCounterHandler = (button) => {
 
 clickButton.addEventListener("click", () => clickCounterHandler(clickButton));
 
+////
 const keydownTracker = document.querySelector("#keydown-tracker");
 const body = document.querySelector('body');
 
-// should track the last key code pressed , event.code
+// should track the last key code pressed, event.code
 const handleKeydown = (e) => {
-if (e.code === "Enter"){
-  keydownTracker.textContent = `You pressed Enter`;
-} else if (e.code === "KeyA"){
-  keydownTracker.textContent = "You pressed KeyA"
-} else if (e.code === "Space"){
-  keydownTracker.textContent = `You pressed Space`;
-}
+  if (e.code === "Enter"){
+    keydownTracker.textContent = `You pressed Enter`;
+  } else if (e.code === "KeyA"){
+    keydownTracker.textContent = "You pressed KeyA"
+  } else if (e.code === "Space"){
+    keydownTracker.textContent = `You pressed Space`;
+  }
 };
-body.addEventListener("keydown", handleKeydown); // add a event listener on body
 
-// remove click handler in the html +
-// add an event listener and use handler from question 1
+body.addEventListener("keydown", handleKeydown);
+
+////
 const inlineButton = document.querySelector("#inline-example");
-inlineButton.addEventListener("click",() => clickCounterHandler(inlineButton) );
-const handleDelegation = () => {
+inlineButton.addEventListener("click",() => clickCounterHandler(inlineButton));
+
+////
+const div = document.querySelector("#delegation");
+
+const handleDelegation = (e) => {
+  const textAboutClicked = document.querySelector("#delegation-result");
+  if(!e.target.matches("button")){
+    return
+  } else {  
+    textAboutClicked.textContent = e.target.textContent;
+  }
 };
+
+div.addEventListener("click", handleDelegation);
 
 const addNewRandomNumber = () => {
 };
