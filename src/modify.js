9d21dd8a1de5064e1501ec52b56b374d17b4dfc2
Angiegru <angiegrullon01@gmail.com>
2023-12-10 12:28:34 -0500

    Question 2 Complete

--- src/modify.js
+++ src/modify.js
@@ -4,27 +4,35 @@ const clickCounterHandler = () => {
   let clicks = clickButton.getAttribute("data-clicks");
 
   let clicksToNum = Number(clicks);
-  clicksToNum++
- clickButton.setAttribute("data-clicks", clicksToNum.toString());
- if (clicksToNum === 0){
-  clickButton.textContent = "I haven't been clicked!";
-} else if (clicksToNum === 1){
-  clickButton.textContent = "I've been clicked 1 time.";
-} else {
-  clickButton.textContent =`I've been clicked ${clicksToNum} times!`;
-}
-}
+    clicksToNum++
+  clickButton.setAttribute("data-clicks", clicksToNum.toString());
+  if (clicksToNum === 0){
+    clickButton.textContent = "I haven't been clicked!";
+  } else if (clicksToNum === 1){
+    clickButton.textContent = "I've been clicked 1 time.";
+  } else {
+    clickButton.textContent =`I've been clicked ${clicksToNum} times!`;
+  }
+};
 
 clickButton.addEventListener("click", clickCounterHandler);
 
-// get access to button's "data-clicks" attribute, turn string into num +1 then back to string
+const keydownTracker = document.querySelector("#keydown-tracker");
+const body = document.querySelector('body');
 
-
-
-const handleKeydown = () => {
-//   const pTag = document.querySelector("#keydown-tracker");
-//   pTag.textContent = `You pressed ${event.code}`;
+// add a event listener on body
+const handleKeydown = (e) => {
+// should track the last key code pressed , event.key
+// modify the textcontent of the `p` tag with an id of `keydown-tracker` with 'You pressed KeyA', 'You pressed Space', 'You pressed Enter'
+if (e.code === "Enter"){
+  keydownTracker.textContent = `You pressed Enter`;
+} else if (e.code === "KeyA"){
+  keydownTracker.textContent = "You pressed KeyA"
+} else if (e.code === "Space"){
+  keydownTracker.textContent = `You pressed Space`;
+}
 };
+body.addEventListener("keydown", handleKeydown);
 
 const handleDelegation = () => {
   // const resultSpan = document.querySelector('#delegation-result');
